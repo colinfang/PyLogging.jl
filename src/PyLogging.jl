c33aa2be30aa6c200e191e240286c39ffa9806bc
colinfang <colinfang@gambitresearch.com>
2016-04-12 19:10:25 +0100

    Reduce number of PyObjects

--- src/PyLogging.jl
+++ src/PyLogging.jl
@@ -16,7 +16,19 @@ const NOTSET = logging.NOTSET
 
 immutable Logger
 	logger::PyObject
+	# The following are cache so that we don't have to create many pyobjects.
+	isEnabledFor::PyObject
+	_log::PyObject
+	
+	function Logger(logger::PyObject)
+		new(
+			logger,
+			logger[:isEnabledFor]
+			logger[:_log]
+		)
+	end
 end
+
 
 
 function getLogger()
@@ -37,40 +49,40 @@ end
 
 
 function _log(logger::Logger, level::Int, msg::AbstractString)
-	logger.logger[:_log](level, msg, [])
+	logger._log(level, msg, [])
 end
 
 
 function critical(logger::Logger, msg::AbstractString)
-	if logger.logger[:isEnabledFor](CRITICAL)
+	if logger.isEnabledFor(CRITICAL)
 		_log(logger, CRITICAL, msg)
 	end
 end
 
 
 function error(logger::Logger, msg::AbstractString)
-	if logger.logger[:isEnabledFor](ERROR)
+	if logger.isEnabledFor(ERROR)
 		_log(logger, ERROR, msg)
 	end
 end
 
 
 function warning(logger::Logger, msg::AbstractString)
-	if logger.logger[:isEnabledFor](WARNING)
+	if logger.isEnabledFor(WARNING)
 		_log(logger, WARNING, msg)
 	end
 end
 
 
 function info(logger::Logger, msg::AbstractString)
-	if logger.logger[:isEnabledFor](INFO)
+	if logger.isEnabledFor(INFO)
 		_log(logger, INFO, msg)
 	end
 end
 
 
 function debug(logger::Logger, msg::AbstractString)
-	if logger.logger[:isEnabledFor](DEBUG)
+	if logger.isEnabledFor(DEBUG)
 		_log(logger, DEBUG, msg)
 	end
 end
@@ -79,7 +91,7 @@ end
 macro critical(logger, msg)
 	quote
 		x = $(esc(logger))
-		if x.logger[:isEnabledFor](CRITICAL)
+		if x.isEnabledFor(CRITICAL)
 			_log(x, CRITICAL, $(esc(msg)))
 		end
 	end
@@ -89,7 +101,7 @@ end
 macro error(logger, msg)
 	quote
 		x = $(esc(logger))
-		if x.logger[:isEnabledFor](ERROR)
+		if x.isEnabledFor(ERROR)
 			_log(x, ERROR, $(esc(msg)))
 		end
 	end
@@ -99,7 +111,7 @@ end
 macro warning(logger, msg)
 	quote
 		x = $(esc(logger))
-		if x.logger[:isEnabledFor](WARNING)
+		if x.isEnabledFor(WARNING)
 			_log(x, WARNING, $(esc(msg)))
 		end
 	end
@@ -109,7 +121,7 @@ end
 macro info(logger, msg)
 	quote
 		x = $(esc(logger))
-		if x.logger[:isEnabledFor](INFO)
+		if x.isEnabledFor(INFO)
 			_log(x, INFO, $(esc(msg)))
 		end
 	end
@@ -119,7 +131,7 @@ end
 macro debug(logger, msg)
 	quote
 		x = $(esc(logger))
-		if x.logger[:isEnabledFor](DEBUG)
+		if x.isEnabledFor(DEBUG)
 			_log(x, DEBUG, $(esc(msg)))
 		end
 	end
